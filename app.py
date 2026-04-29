7b1bbe268bf5c6039495e3627c8603abbdfeb0a3
AgentIrishka-Sketch <serdyukova.irina@gmail.com>
2026-04-29 13:22:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ with ev_col2:
 
 with ev_col3:
     # inject a tiny gap above the button to nudge it up visually
-    st.markdown("<div style='line-height: 2.1em'></div>", unsafe_allow_html=True)
+    st.markdown("<div style='line-height: 100em'></div>", unsafe_allow_html=True)
     add_clicked = st.button("+ Add", key="add_event")
     
 # --- Color picker: selectbox + colored preview ---
